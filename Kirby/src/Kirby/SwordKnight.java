b532566809e558f3d239eefa4ddeb0703b043a4c
Jasmine Emerson <jazafrazzle@gmail.com>
2016-12-15 21:04:08 -0800

    finished attacks for sword knight

--- Kirby/src/Kirby/SwordKnight.java
+++ Kirby/src/Kirby/SwordKnight.java
@@ -1,8 +1,14 @@
+
+
 package Kirby;
 
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Random;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 
 import jig.Entity;
 import jig.ResourceManager;
@@ -12,9 +18,14 @@ import jig.Vector;
 	public static final String[] facingImages = 
 		{
 			KirbyGame.SWORDKNIGHT_RIGHT,
-			KirbyGame.SWORDKNIGHT_LEFT,
+			KirbyGame.SWORDKNIGHT_LEFT
+		};
+	
+	public static final String[] attackingImages = 
+		{
 			KirbyGame.SWORDKNIGHT_ATTACK_R,
 			KirbyGame.SWORDKNIGHT_ATTACK_L
+			
 		};
 	
 	private boolean inNest;
@@ -24,66 +35,60 @@ import jig.Vector;
 	private boolean firstPath;
 	private int waitTime;
 	Random rand = new Random();
+	
+	private int attackTime;
+	private boolean attackState;
 
 	public SwordKnight(final float x, final float y) {
 		super(x, y, facingImages, LEFT_WALK);
 		setVelocity(new Vector(0, 0));
-		inNest = false;
-		firstPath = true;
-		waitTime = rand.nextInt(200);
+		attackState = false;
 	}
 
 	
 	public void setMoving(KirbyGame bg) {
-		if ((hasPassed() || firstPath) && waitTime <= 0) {
-			if (firstPath)
-				firstPath = false;
-			Vertex v = bg.vPos.get(vPos.toString());
-			int r = rand.nextInt(v.neighbors.size());
-			int c = 0;
-			for (Vertex n : v.getNeighbors()) {
-				if (c == r) nextPos = n;
-				c++;
-			}
-			if (nextPos.getX() > vPos.getX()) {
-				setVelocity(new Vector(.07f, 0f));
-				direction = "right";
-			} else if (nextPos.getX() < vPos.getX()) {
-				setVelocity(new Vector(-.07f, 0f));
-				direction = "left";
-			} else if (nextPos.getY() > vPos.getY()) {
-				setVelocity(new Vector(0f, .07f));
-				direction = "below";
-			} else {
-				setVelocity(new Vector(0f, -.07f));
-				direction = "above";
-			}
-			waitTime = rand.nextInt(1);
-		}
-		if (hasPassed()) {
-			setVelocity(new Vector(0f, 0f));
-			vPos = nextPos;
-		}
-		if (waitTime > 0)
-			waitTime--;
 	}	
 	
-	private boolean hasPassed() {
-		if (direction != null && nextPos != null) {
-			if (direction.equals("left"))
-				return getPosition().getX() <= nextPos.getX();
-			else if (direction.equals("right"))
-				return getPosition().getX() >= nextPos.getX();
-			else if (direction.equals("above"))
-				return getPosition().getY() <= nextPos.getY();
-			else
-				return getPosition().getY() >= nextPos.getY();
+	public int retFacing() {
+    	return super.facingRight() ? 0 : 1;
+    }
+	
+	@Override
+	public void render(Graphics g, float offsetX, float offsetY) throws SlickException {
+		Image i;
+		if (attackState){
+			i = new Image(attackingImages[super.getFacing()]);
+			i.draw(super.getX() - 40 - offsetX, super.getY() + 4 - offsetY);
 		}
-		return false;
+		else{
+			i = new Image(facingImages[super.getFacing()]);
+			i.draw(super.getX() - 4 - offsetX, super.getY() + 4 - offsetY);
+		}
 	}
 	
 	@Override
 	public int getEnemyType() {
 		return SWORDKNIGHT;
 	}
+	
+	public void attack(KirbyGame bg) {
+//		super.setFacingImages(attackingImages);
+//		super.setCurImage(attackingImages[super.getFacing()]);
+//		super.setPosition(super.getX(), super.getY());
+		
+		attackState = true;
+		attackTime = 20;
+		
+	}
+
+	@Override
+	public void update(final int delta) {
+		translate(super.getVelocity().scale(delta));
+		if (attackTime > 0) {
+			attackTime--;
+		} else {
+			attackState = false;
+		}
+	}
 }
+
